408a8c00bca730c954f6aa491896005805845f0e
myemets <for.education1809@gmail.com>
2024-06-28 16:46:16 +0300

    make build

---
+++ index.05a9f829.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall"),l=t.clientHeight/2-e.clientHeight/2,i=t.clientWidth/2-e.clientWidth/2;e.style.top=l+"px",e.style.left=i+"px";
+//# sourceMappingURL=index.05a9f829.js.map
